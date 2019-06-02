f0c975f720b052c284b05ec51fa3d9c0cd5e52c8
Tony Tia <webmaster@tonytia.com>
2019-06-02 14:57:16 -0700

    update netlify

---
+++ Front-End/build/precache-manifest.b6c81612f8a7084542106a980e6faf13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff45a94d76fed5806cef9e602c382bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de7a197a52eaba4da1bf",
+    "url": "/static/css/2.d7346376.chunk.css"
+  },
+  {
+    "revision": "90f47b9c1dc4fce22bcf",
+    "url": "/static/css/main.f206bbdf.chunk.css"
+  },
+  {
+    "revision": "de7a197a52eaba4da1bf",
+    "url": "/static/js/2.21cbe2cb.chunk.js"
+  },
+  {
+    "revision": "90f47b9c1dc4fce22bcf",
+    "url": "/static/js/main.d9a23a96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9fcb606f6c18e8846a849b31aa3bcf24",
+    "url": "/static/media/aapte.9fcb606f.png"
+  },
+  {
+    "revision": "d23c98048b5b08a3818e5aba9fd1f955",
+    "url": "/static/media/jsolis.d23c9804.png"
+  },
+  {
+    "revision": "f98eba7d09946765a430a4e9ff39ec2d",
+    "url": "/static/media/ttia.f98eba7d.png"
+  }
+]);
